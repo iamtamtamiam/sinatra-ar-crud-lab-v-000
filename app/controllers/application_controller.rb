a3d5a6eb9c4d3e98483c5e3c254e179a2c7472bb
Tam Vu <vutam_a@hotmail.com>
2019-11-27 19:32:01 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,11 +32,10 @@ class ApplicationController < Sinatra::Base
   end
   
   get '/articles/:id/edit' do
-    
-    
+    @article = Article.find(params[:id])
     erb :edit
   end 
   
-  
+  patch ''
   
 end
