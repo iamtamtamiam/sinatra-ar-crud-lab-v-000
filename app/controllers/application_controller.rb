48285d6f40bc2a9961647b33f132177f78acc1bb
Tam Vu <vutam_a@hotmail.com>
2019-11-27 20:02:39 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,9 +38,9 @@ class ApplicationController < Sinatra::Base
   
   patch '/articles/:id' do 
      @article = Article.find(params[:id])
-     @article.update(params[:article])
-     @article.save
-     @article
+     @article = @article.update(params[:article])
+     
+     
      redirect to "/articles/#{ @article.id }"
   end 
   
